192de3537dfc4f42dad26c3b051fb0605def303b
Lolhostis <72961722+Lolhostis@users.noreply.github.com>
2021-11-22 21:05:00 +0100

    Remove older files

--- Vues/footer.php
+++
@@ -1,8 +0,0 @@
-    <footer class="text-muted">
-      <div class="container">
-        <p class="float-end">
-          <a href="#">Back to top</a>
-        </p>
-        <p class="mb-1">Synapse 2021</p>
-      </div>
-    </footer>
