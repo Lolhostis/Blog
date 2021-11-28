9b06a784f1594d74c6156338b173c69c79ec1c1f
Arnaud63 <a.allemand0@gmail.com>
2021-11-28 16:33:14 +0100

    test_CommentModel.php fully working

--- Config/Validation.php
+++ Config/Validation.php
@@ -4,34 +4,56 @@ class Validation {
 
 static function val_action($action) {
 
-if (!isset($action)) {
-throw new Exception('pas d\'action');
-    //on pourrait aussi utiliser
-//$action = $_GET['action'] ?? 'no';
-    // This is equivalent to:
-    //$action =  if (isset($_GET['action'])) $action=$_GET['action']  else $action='no';
-}
-}
+	if (!isset($action)) {
+		throw new Exception('pas d\'action');
+		//on pourrait aussi utiliser
+		//$action = $_GET['action'] ?? 'no';
+		// This is equivalent to:
+		//$action =  if (isset($_GET['action'])) $action=$_GET['action']  else $action='no';
+		}
+	}
 
-    static function val_form(string &$nom, string &$age, array &$dVueEreur) {
-
-        if (!isset($nom)||$nom=="") {
-            $dVueEreur[] =	"pas de nom";
-            $nom="";
+	static function val_form_comment_consult(string &$id, string &$errors) {
+        if (!isset($id)||$id==""||!filter_var($id, FILTER_VALIDATE_INT)) {
+            $errors = $errors."No valid id</br>";
+            $id="";
         }
+    }
 
-        if ($nom != filter_var($nom, FILTER_SANITIZE_STRING))
-        {
-            $dVueEreur[] =	"testative d'injection de code (attaque sécurité)";
-            $nom="";
-
+    static function val_form_comment_add(string &$id, string &$text, string &$date, string &$login_user, string &$id_news,string &$errors) {
+        if (!isset($id)||$id==""||!filter_var($id, FILTER_VALIDATE_INT)) {
+            $errors = $errors."No valid id</br>";
+            $id="";
         }
-
-        if (!isset($age)||$age==""||!filter_var($age, FILTER_VALIDATE_INT)) {
-            $dVueEreur[] =	"pas d'age ";
-            $age=0;
+		
+		if (!isset($text)||$text=="") {
+            $errors = $errors."No valid text</br>";
+            $text="";
         }
+		else {
+			$text = filter_var($text, FILTER_SANITIZE_STRING);
+		}
 
+        if (!isset($date)||$date=="") {
+            $errors = $errors."No valid date</br>";
+            $date="";
+        }
+		else {
+			$date = filter_var($date, FILTER_SANITIZE_STRING);
+		}
+		
+		if (!isset($login_user)||$login_user=="") {
+            $errors = $errors."No valid login</br>";
+            $login_user="";
+        }
+		else {
+			$login_user = filter_var($login_user, FILTER_SANITIZE_STRING);
+		}
+		
+		if (!isset($id_news)||$id_news==""||!filter_var($id_news, FILTER_VALIDATE_INT)) {
+            $errors = $errors."No valid id_news</br>";
+            $id_news="";
+        }
     }
 
 }
